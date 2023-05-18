b53194b508cf89604b9d17fbb5e741edd6dc71eb
Tudzy <narongsak5az@gmail.com>
2023-05-18 14:51:18 +0700

    edit review controllers

--- controllers/reviewControllers.js
+++ controllers/reviewControllers.js
@@ -4,7 +4,7 @@ const db = require("../models")
 const Review = db.review
 
 // 1 add review
-const addReview = async (res, req) => {
+const addReview = async (req, res) => {
     let data = {
         rating: req.body.rating,
         descriotion: req.body.descriotion,
@@ -13,17 +13,17 @@ const addReview = async (res, req) => {
         const review = await Review.create(data)
         res.status(200).send(review)
     } catch (error) {
-        res.status(500).send("found add review")
+        res.status(500).send("Failed to add review");
     }
 }
 
 // 2 get all review
-const getAllreview = async (res, req) => {
+const getAllreview = async(req, res) => {
     try {
         const review = await Review.findAll({})
         res.status(200).send(review)
     } catch (error) {
-        res.status(500).send("found get review")
+        res.status(500).send("Failed to get review");
     }
 }
 
--- routers/productRouters.js
+++ routers/productRouters.js
@@ -1,10 +1,19 @@
 const productControllers = require('../controllers/productControllers.js')
+const reviewControllers = require('../controllers/reviewControllers.js')
+
+// router
 const router = require('express').Router()
 
+// use controllers
 router.post('/addProduct', productControllers.addProduct)
 router.get('/getAllproduct', productControllers.getAllproduct)
 router.get('/getPublishedProduct', productControllers.getPublishedProduct)
 
+// review controller
+router.get("/getAllreview",reviewControllers.addReview)
+router.post("/addReview",reviewControllers.addReview)
+
+// product controller
 router.get('/:id', productControllers.getOneproduct)
 router.put('/:id', productControllers.updateProduct)
 router.delete('/:id', productControllers.deleteProduct)
